840a3d8b5bf5ef7b3ebee013de781016687da6ae
zhichaoGuo <cn.guozhichao@gmail.com>
2022-04-09 15:44:24 +0800

    完善函数

--- TestCase/TestUpgrade.py
+++ TestCase/TestUpgrade.py
@@ -23,11 +23,11 @@ class TestApUpgrade:
     @pytest.mark.parametrize('rom_version', 'assist_rom_version', [f["rom_version"], f["assist_rom_version"]])
     def test_ap_https(self, rom_version, assist_rom_version):
         DUT = VoipDevice('10.20.0.32', '5060', 'admin', 'admin')
-        DUT.set_rom_path('https://10.3.3.49/fw/abyss/server1')
+        DUT.set_rom_path('https://10.3.3.49:8668/fw/abyss/server1')
         auto_provision(DUT)
         sleep_phone_up(DUT)
         assert DUT.version() == rom_version
-        DUT.set_rom_path('https://10.3.3.49/fw/abyss/server2')
+        DUT.set_rom_path('https://10.3.3.49:8668/fw/abyss/server2')
         auto_provision(DUT)
         sleep_phone_up(DUT)
         assert DUT.version() == assist_rom_version
--- TestLib/VoipDevice.py
+++ TestLib/VoipDevice.py
@@ -1,3 +1,5 @@
+import re
+
 from TestLib import hl_request
 from TestLib.Utils import postFormToDevice
 from TestLib.cfg import CfgGenerator
@@ -9,16 +11,31 @@ class VoipDevice:
         self.port = port
         self.user = user
         self.password = password
-        self.version = self._get_version()
-        self.model = self._get_model()
+        text = hl_request('GET', 'http://%s/index.htm' % ip, auth=(user, password), timeout=1).text
+        self.version = self._get_version(text)
+        self.model = self._get_model(text)
+        self.mac = self._get_mac(text)
 
-    def _get_version(self):
-        version = ''
-        return version
+    def _get_version(self, text):
+        re_1 = 'ROM--'
+        re_2 = '\('
+        text = text[re.search(re_1, text, re.U).span()[1]:]
+        text = text[:re.search(re_2, text, re.U).span()[0]]
+        return text
 
-    def _get_model(self):
-        model = ''
-        return model
+    def _get_model(self, text):
+        re_1 = 'jscs.product_type'
+        re_2 = '</td>'
+        text = text[re.search(re_1, text, re.U).span()[1] + 44:]
+        text = text[:re.search(re_2, text, re.U).span()[0]]
+        return text
+
+    def _get_mac(self,text):
+        re_1 = 'jscs.mac_address'
+        re_2 = '</td>'
+        text = text[re.search(re_1, text, re.U).span()[1] + 45:]
+        text = text[:re.search(re_2, text, re.U).span()[0]]
+        return text
 
     def sendKey(self, keyCode: str):
         url = "http://%s:%s/Phone_ActionURL&Command=3&key=%s" % (self.ip, self.port, keyCode)
